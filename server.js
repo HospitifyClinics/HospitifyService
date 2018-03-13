b1a734e8601cdaa0543b0c32cf8ff14759a258ac
Abdullah Rababah <arababah@ebay.com>
2018-03-13 02:33:22 -0700

    making changes ... to test AWS deployment pipeline

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const db = mongoose.connection;
 
 //Bind connection to error event (to get notification of connection errors)
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-
+console.log('making changes ... to test AWS deployment pipeline');
 
 // Create our Express application
 const app = express();
